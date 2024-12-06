ab203615f0c721165d5e7deeb95a01ae2f05526b
Garnet <garnet.cheng@vtsd.com>
2024-12-06 12:56:25 -0500

    1-2-4 Day 4

--- 1-2-4/main.py
+++ 1-2-4/main.py
@@ -4,8 +4,9 @@ import random as rand
 # Intialize Variables
 wn = trtl.Screen()
 maze_painter = trtl.Turtle()
-path_width = 20
-length = 80
+path_width = 30
+global length
+length = 35
 # Setup Turtle
 maze_painter.left(90)
 maze_painter.pensize(5)
@@ -17,26 +18,26 @@ maze_painter.speed(0)
 # Turn Left
 # increment Length
 # Repeat
-
-door = rand.randint(path_width*2, (length - path_width*2))
-barrier = rand.randint(path_width*2, (length - path_width*2))
-
 def draw_barrier():
     maze_painter.right(90)
     maze_painter.forward(path_width)
     maze_painter.backward(path_width)
     maze_painter.left(90)
 
-for i in range(21):
-    maze_painter.forward(length/3)
+for wall in range(21):
+    gap_space = rand.randint(0, length - path_width)
+    door_space =  0
+    maze_painter.forward(gap_space)
     maze_painter.penup()
     maze_painter.forward(path_width)
     maze_painter.pendown()
-    draw_barrier()
-    maze_painter.forward(length - path_width - length/3)
+    if(wall > 5):
+        door_space = rand.randint(0, length - gap_space - path_width)
+        maze_painter.forward(door_space)
+        draw_barrier()
+    maze_painter.forward(length - gap_space - path_width - door_space)
     maze_painter.left(90)
     length += 15
-
 
 
 
