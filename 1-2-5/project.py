2f07e51a583c768dd02d30b8367394971603dc66
Garnet <garnet.cheng@vtsd.com>
2024-12-12 12:56:35 -0500

    1-2-5 Day 3

--- 1-2-5/project.py
+++ 1-2-5/project.py
@@ -4,18 +4,29 @@ import random as rand
 #variable creation and named them all
 painter = trtl.Turtle()
 wn = trtl.Screen()
+wn.title("Pong Game")
+wn.setup(width = 1000, height = 600)
 painter.screen.bgcolor('black')
 painter.speed('fastest')
 
+#This is for the creation of the middle lines of the ping game which show which side its on
 def middle_lines():
     painter.color('white')
     painter.hideturtle()
     painter.penup()
-    painter.setpos(0,500 )
-    painter.pendown()
-    painter.pensize(20)
-    painter.right(90)
-    painter.forward(30)
+    painter.setpos(0,300 )
+    painter.setheading(270)
+    painter.pensize(5)
+
+for _ in range (30):
+        painter.pendown()
+        painter.forward(10)
+        painter.penup()
+        painter.forward(10)
+
+
+
+
 
 
 
